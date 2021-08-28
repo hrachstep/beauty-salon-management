4018e7d4c5ce731cf4c082ed11038b9562e84b93
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-08-28 13:18:20 -0300

    feat: add repository for ServiceType

---
+++ src/infrastructure/repositories/ServiceTypeRepository.ts
@@ -0,0 +1,79 @@
+import {
+  collection,
+  CollectionReference,
+  doc,
+  Firestore,
+  getDoc,
+  getDocs,
+  limit,
+  query,
+  setDoc,
+  where,
+} from 'firebase/firestore';
+
+import { ServiceType } from '@domain/entities/ServiceType';
+import { IServiceTypeRepository } from '@domain/interfaces/IServiceTypeRepository';
+
+import { Firebase } from './Firebase';
+
+export class ServiceTypeRepository implements IServiceTypeRepository {
+  readonly db: Firestore;
+  readonly tableName: string;
+  readonly table: CollectionReference;
+
+  constructor() {
+    this.db = Firebase.storage;
+    this.tableName = 'serviceTypes';
+    this.table = collection(this.db, this.tableName);
+  }
+
+  async create({ id, name }: ServiceType): Promise<ServiceType> {
+    await setDoc(doc(this.table, id), {
+      id,
+      name,
+    });
+
+    return { id, name };
+  }
+
+  async update({ id, name }: ServiceType): Promise<ServiceType> {
+    await setDoc(doc(this.table, id), {
+      id,
+      name,
+    });
+
+    return { id, name };
+  }
+
+  async findAll(): Promise<ServiceType[]> {
+    const snapshot = await getDocs(query(this.table));
+
+    if (snapshot.empty) return [];
+
+    return snapshot.docs.map((item) => item.data() as ServiceType);
+  }
+
+  async findById(id: string): Promise<ServiceType> {
+    const snapshot = await getDoc(doc(this.table, id));
+
+    if (!snapshot.exists) return null;
+
+    return snapshot.data() as ServiceType;
+  }
+
+  async findByIds(ids: string[]): Promise<ServiceType[]> {
+    const snapshot = await getDocs(query(this.table, where('id', 'in', ids)));
+
+    if (snapshot.empty) return null;
+
+    return snapshot.docs.map((item) => item.data() as ServiceType);
+  }
+
+  async findByName(name: string): Promise<ServiceType> {
+    const snapshot = await getDocs(query(this.table, where('name', '==', name), limit(1)));
+
+    if (snapshot.empty) return null;
+
+    return snapshot.docs[0].data() as ServiceType;
+  }
+}
