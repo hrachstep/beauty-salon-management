f94676298966c4f56c9c40f17bd14aafc536ee55
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-09-01 20:09:22 -0300

    feat: change ApiError to allow get instance property status code

--- src/shared/errors/ApiError.ts
+++ src/shared/errors/ApiError.ts
@@ -1,5 +1,8 @@
 export class ApiError extends Error {
-  constructor(message: string, private statusCode = 400) {
+  readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
     super(message);
+    this.statusCode = statusCode;
   }
 }
