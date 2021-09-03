293cc1118a1b19953ab065c8963725ca9caa8e0a
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-09-03 16:25:05 -0300

    feat: add route to list Service by Id

--- src/main/config/routes/services.routes.ts
+++ src/main/config/routes/services.routes.ts
@@ -1,15 +1,22 @@
 import { Router } from 'express';
 
+import { ListServiceByIdController } from '@main/controllers/listServiceById/ListServiceByIdController';
 import { ListServicesController } from '@main/controllers/listServices/ListServicesController';
 
 import { checkErrorMiddleware } from '../middlewares/checkErrorMiddleware';
 
 const router = Router();
 const listServicesControler = new ListServicesController();
+const listServiceById = new ListServiceByIdController();
 
 router.get('/',
   listServicesControler.validation(),
   checkErrorMiddleware,
   listServicesControler.handle.bind(listServicesControler));
+
+router.get('/:id',
+  listServiceById.validate(),
+  checkErrorMiddleware,
+  listServiceById.handle.bind(listServiceById));
 
 export default router;
