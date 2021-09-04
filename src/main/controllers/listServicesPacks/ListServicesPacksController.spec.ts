82aff356acb364a6d679d84ff99db5cc1fe298e9
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-09-04 13:04:40 -0300

    test: add tests for list all services packs controller

---
+++ src/main/controllers/listServicesPacks/ListServicesPacksController.spec.ts
@@ -0,0 +1,22 @@
+import request from 'supertest';
+
+import { createApp } from '@main/config/app';
+
+describe('List Services Packs controller', () => {
+  const app = createApp();
+  const route = '/services-packs';
+
+  it('should return status 400 if "month" is invalid', async () => {
+    const response = await request(app)
+      .get(`${route}?month=incorrect`);
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  it('should return status 200 on success', async () => {
+    const response = await request(app)
+      .get(`${route}`);
+
+    expect(response.statusCode).toBe(200);
+  });
+});
