eb0fb21f71025734458d3ddd3bf5735c74b4647c
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-09-04 12:44:11 -0300

    feat: add route for add service done on services pack

--- src/main/config/routes/servicesPack.routes.ts
+++ src/main/config/routes/servicesPack.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { AddServiceDoneOnServicesPackController } from '@main/controllers/addServiceDoneOnServicesPack/AddServiceDoneOnServicesPackController';
 import { CreateServicesPackController } from '@main/controllers/createServicesPack/CreateServicesPackController';
 
 import { checkErrorMiddleware } from '../middlewares/checkErrorMiddleware';
@@ -7,10 +8,16 @@ import { checkErrorMiddleware } from '../middlewares/checkErrorMiddleware';
 const router = Router();
 
 const createServicesPackController = new CreateServicesPackController();
+const addServiceDoneOnServicesPackController = new AddServiceDoneOnServicesPackController();
 
 router.post('/',
   createServicesPackController.validate(),
   checkErrorMiddleware,
   createServicesPackController.handle.bind(createServicesPackController));
+
+router.post('/:id/services',
+  addServiceDoneOnServicesPackController.validate(),
+  checkErrorMiddleware,
+  addServiceDoneOnServicesPackController.handle.bind(addServiceDoneOnServicesPackController));
 
 export default router;
