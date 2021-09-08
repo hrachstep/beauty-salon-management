49dd068cc401ca7734f272011505f4d5b41557e4
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-09-08 17:25:43 -0300

    feat: add route to delete a services pack

--- src/main/config/routes/servicesPack.routes.ts
+++ src/main/config/routes/servicesPack.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { AddServiceDoneOnServicesPackController } from '@main/controllers/addServiceDoneOnServicesPack/AddServiceDoneOnServicesPackController';
 import { CreateServicesPackController } from '@main/controllers/createServicesPack/CreateServicesPackController';
+import { DeleteServicesPackController } from '@main/controllers/deleteServicesPack/DeleteServicesPackController';
 import { ListServicesPacksController } from '@main/controllers/listServicesPacks/ListServicesPacksController';
 
 import { checkErrorMiddleware } from '../middlewares/checkErrorMiddleware';
@@ -11,6 +12,7 @@ const router = Router();
 const createServicesPackController = new CreateServicesPackController();
 const addServiceDoneOnServicesPackController = new AddServiceDoneOnServicesPackController();
 const listServicesPacksController = new ListServicesPacksController();
+const deleteServicesPackController = new DeleteServicesPackController();
 
 router.get('/',
   listServicesPacksController.validate(),
@@ -26,5 +28,7 @@ router.post('/:id/services',
   addServiceDoneOnServicesPackController.validate(),
   checkErrorMiddleware,
   addServiceDoneOnServicesPackController.handle.bind(addServiceDoneOnServicesPackController));
+
+router.delete('/:id', deleteServicesPackController.handle.bind(deleteServicesPackController));
 
 export default router;
