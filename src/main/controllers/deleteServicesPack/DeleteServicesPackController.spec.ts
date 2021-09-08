fdf592866b040becff4a1329542f8b6cabd4ce69
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-09-08 17:25:55 -0300

    test: add test to route for delete a services pack

---
+++ src/main/controllers/deleteServicesPack/DeleteServicesPackController.spec.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+
+import { createApp } from '@main/config/app';
+
+describe('Delete Service controller', () => {
+  const app = createApp();
+  const id = '1234';
+
+  it('should send status 204 with no response', async () => {
+    const response = await request(app)
+      .delete(`/services-packs/${id}`);
+
+    expect(response.statusCode).toBe(204);
+  });
+});
