52f66f2247bb78b8890246c690f70cc150aeb8df
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-09-08 17:34:37 -0300

    feat: add route to delete a service type

--- src/main/config/routes/serviceTypes.routes.ts
+++ src/main/config/routes/serviceTypes.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { checkErrorMiddleware } from '@main/config/middlewares/checkErrorMiddleware';
 import { CreateServiceTypeController } from '@main/controllers/createServiceType/CreateServiceTypeController';
+import { DeleteServiceTypeController } from '@main/controllers/deleteServiceType/DeleteServiceTypeController';
 import { ListServiceTypesController } from '@main/controllers/listServiceTypes/ListServiceTypesController';
 
 const createServiceTypeController = new CreateServiceTypeController();
 const listServiceTypesController = new ListServiceTypesController();
+const deleteServiceTypeController = new DeleteServiceTypeController();
 
 const router = Router();
 
@@ -15,5 +17,7 @@ router.post('/',
   createServiceTypeController.validate(),
   checkErrorMiddleware,
   createServiceTypeController.handle.bind(createServiceTypeController));
+
+router.delete('/:id', deleteServiceTypeController.handle.bind(deleteServiceTypeController));
 
 export default router;
