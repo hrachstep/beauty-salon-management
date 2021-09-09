5f3a1488de4b2c6a0a7e67981dfcb9ddbd1bac3c
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-09-09 18:35:22 -0300

    feat: add dependency injection for Storage Provider implementation

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,6 +3,8 @@ import { container } from 'tsyringe';
 import { IServiceRepository } from '@domain/interfaces/IServiceRepository';
 import { IServicesPackRepository } from '@domain/interfaces/IServicesPackRepository';
 import { IServiceTypeRepository } from '@domain/interfaces/IServiceTypeRepository';
+import { IStorageProvider } from '@domain/interfaces/IStorageProvider';
+import { FirebaseStorageProvider } from '@infrastructure/repositories/FirebaseStorageProvider';
 import { ServiceRepository } from '@infrastructure/repositories/ServiceRepository';
 import { ServicesPackRepository } from '@infrastructure/repositories/ServicesPackRepository';
 import { ServiceTypeRepository } from '@infrastructure/repositories/ServiceTypeRepository';
@@ -10,3 +12,4 @@ import { ServiceTypeRepository } from '@infrastructure/repositories/ServiceTypeR
 container.registerSingleton<IServiceRepository>('ServiceRepository', ServiceRepository);
 container.registerSingleton<IServiceTypeRepository>('ServiceTypeRepository', ServiceTypeRepository);
 container.registerSingleton<IServicesPackRepository>('ServicesPackRepository', ServicesPackRepository);
+container.registerSingleton<IStorageProvider>('StorageProvider', FirebaseStorageProvider);
