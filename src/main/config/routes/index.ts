f70b79d015be05b09d422e634bbc7a9d3f7f6e84
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-09-11 11:57:08 -0300

    feat: add ensureAuthentication to every route

--- src/main/config/routes/index.ts
+++ src/main/config/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import servicesRoutes from './services.routes';
 import servicesPackRoutes from './servicesPack.routes';
 import serviceTypesRoutes from './serviceTypes.routes';
 
 const router = Router();
 
+router.use(ensureAuthenticated);
 router.use('/service-types', serviceTypesRoutes);
 router.use('/services', servicesRoutes);
 router.use('/services-packs', servicesPackRoutes);
