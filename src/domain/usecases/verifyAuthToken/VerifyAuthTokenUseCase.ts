d5009d25a4d4eaa5e531b2639379af7c4713ee2e
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-09-11 11:38:03 -0300

    feat: add dependency injection for Auth Provider

--- src/domain/usecases/verifyAuthToken/VerifyAuthTokenUseCase.ts
+++ src/domain/usecases/verifyAuthToken/VerifyAuthTokenUseCase.ts
@@ -1,10 +1,16 @@
+import { inject, injectable } from 'tsyringe';
+
 import { Auth } from '@domain/entities/Auth';
 import { User } from '@domain/entities/User';
 import { IAuthProvider } from '@domain/interfaces/IAuthProvider';
 import { ApiError } from '@shared/errors/ApiError';
 
+@injectable()
 export class VerifyAuthTokenUseCase {
-  constructor(private readonly authProvider: IAuthProvider) {}
+  constructor(
+    @inject('AuthProvider')
+    private readonly authProvider: IAuthProvider,
+  ) {}
 
   async execute({ token, signInMethod, providerId }: Auth): Promise<User> {
     const user = await this.authProvider.verify({
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,11 @@
 import { container } from 'tsyringe';
 
+import { IAuthProvider } from '@domain/interfaces/IAuthProvider';
 import { IServiceRepository } from '@domain/interfaces/IServiceRepository';
 import { IServicesPackRepository } from '@domain/interfaces/IServicesPackRepository';
 import { IServiceTypeRepository } from '@domain/interfaces/IServiceTypeRepository';
 import { IStorageProvider } from '@domain/interfaces/IStorageProvider';
+import { FirebaseAuthProvider } from '@infrastructure/auth/FirebaseAuthProvider';
 import { FirebaseStorageProvider } from '@infrastructure/providers/FirebaseStorageProvider';
 import { ServiceRepository } from '@infrastructure/repositories/ServiceRepository';
 import { ServicesPackRepository } from '@infrastructure/repositories/ServicesPackRepository';
@@ -13,3 +15,4 @@ container.registerSingleton<IServiceRepository>('ServiceRepository', ServiceRepo
 container.registerSingleton<IServiceTypeRepository>('ServiceTypeRepository', ServiceTypeRepository);
 container.registerSingleton<IServicesPackRepository>('ServicesPackRepository', ServicesPackRepository);
 container.registerSingleton<IStorageProvider>('StorageProvider', FirebaseStorageProvider);
+container.registerSingleton<IAuthProvider>('AuthProvider', FirebaseAuthProvider);
