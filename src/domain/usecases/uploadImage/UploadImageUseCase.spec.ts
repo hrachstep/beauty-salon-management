325b34185a2235efa4394a6c9ae7e61026a53625
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-09-11 12:48:07 -0300

    test: fix error on method parameters on Upload Image usecase

--- src/domain/usecases/uploadImage/UploadImageUseCase.spec.ts
+++ src/domain/usecases/uploadImage/UploadImageUseCase.spec.ts
@@ -9,22 +9,22 @@ describe('Upload Image usecase', () => {
   const folder = 'nails';
   const image = 'image-file';
 
-  const usecase = new UploadImageUseCase(storageProvider, folder);
+  const usecase = new UploadImageUseCase(storageProvider);
 
   it('should receive a image file and upload to an external storage provider', async () => {
-    await usecase.execute(image);
+    await usecase.execute(image, folder);
 
     expect(storageProvider.save).toHaveBeenCalledWith(image, folder);
   });
 
   it('should return a url after image upload', async () => {
-    const result = await usecase.execute(image);
+    const result = await usecase.execute(image, folder);
 
     expect(result).toBeTruthy();
   });
 
   it('should return null if image dont exists', async () => {
-    const result = await usecase.execute(null);
+    const result = await usecase.execute(null, folder);
 
     expect(result).toBeNull();
   });
